da5e2b6b3b50ddfa126047fc515bc9e3b2811cad
Lazizbek <juniordev1005@gmail.com>
2021-06-26 22:23:47 +0500

    vue add vuetify and little changes

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,94 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-btn class="mx-1" text to="/"> Home </v-btn>
+
+      <v-btn class="mx-1" text to="/about"> About </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text @click="loading"> Loading </v-btn>
+
+      <v-btn text @click="$vuetify.theme.dark = !$vuetify.theme.dark">
+        Toggle Theme
+      </v-btn>
+    </v-app-bar>
+
+    <div :class="isLoading ? 'loading' : ''"></div>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  methods: {
+    loading() {
+      this.isLoading = true;
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 4000);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.loading {
+  margin-top: 56px;
+  height: 20px;
+  background-color: #2196F3;
+  animation: loading 2s infinite;
 }
 
-#nav {
-  padding: 30px;
-}
+@keyframes loading {
+  0% {
+    width: 0;
+  }
+  10% {
+    width: 10%;
+  }
+  20% {
+    width: 20%;
+  }
+  40% {
+    width: 40%;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  60% {
+    width: 70%;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  100% {
+    width: 100%;
+  }
 }
 </style>
